cdbaacd5736a06aa70ac6a4924c4101f9d27e240
viral32111 <19510403+viral32111@users.noreply.github.com>
2022-08-05 15:13:53 +0100

    Rewrite for v2
    Now uses optional flags, and has much better error handling.
    
    No longer outputs the URL or size of body.

--- source/healthcheck.go
+++ source/healthcheck.go
@@ -1,97 +1,169 @@
 package main
 
 /* Requirements:
- Needs to be ran with just the URL argument.
- Needs to return only 0 for success, and 1 for failure.
- Needs to output only a single status message.
- Needs to only attempt connections once.
- Needs to have infinite timeout because Docker will handle that for us.
+Needs to be ran with just the URL argument.
+Needs to return only 0 for success, and 1 for failure.
+Needs to output only a single status message.
+Needs to only attempt connections once.
+Needs to have infinite timeout because Docker will handle that for us.
 */
 
 /* Usage:
- hc http://192.168.10.20:5000/metrics
- hc http://abcxyzhiddenservice.onion 127.0.0.1:9150
+$ healthcheck http://localhost:5000/metrics
+SUCCESS, 200 OK
+
+$ healthcheck -expect 204 http://localhost:5000/betrics
+FAILURE, 404 Not Found
+
+$ healthcheck --expect 100 --proxy 127.0.0.1:9050 http://hiddenservice.onion
+SUCCESS, 100 Continue
+
+$ healthcheck --method POST http://example.com/health
+SUCCESS, 200 OK
 */
 
 import (
+	"flag"
 	"fmt"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
-	"io"
+	"strconv"
 	"strings"
+
 	"golang.org/x/net/proxy"
 )
 
 const (
+	PROJECT_NAME = "Container Health Check"
+	PROJECT_VERSION = "2.0.0"
+
 	AUTHOR_NAME = "viral32111"
 	AUTHOR_WEBSITE = "https://viral32111.com"
-
-	VERSION = "1.1.0"
-
-	PROXY_ADDRESS = "127.0.0.1:9050"
 )
 
-
 func main() {
-	argumentCount := len( os.Args[ 1: ] )
 
-	if ( argumentCount < 1 ) {
-		fmt.Printf( "Container Health Check, v%s, by %s (%s)\n", VERSION, AUTHOR_NAME, AUTHOR_WEBSITE )
-		fmt.Fprintf( os.Stderr, "Usage: %s <url> [socks5 proxy]", os.Args[ 0 ] )
+	// Holds the values of the command-line flags, and provides the defaults
+	flagExpectStatusCode := 200
+	flagUseMethod := "GET"
+	flagProxyServer := ""
+
+	// Setup the command-line flags
+	flag.IntVar( &flagExpectStatusCode, "expect", flagExpectStatusCode, "The HTTP status code to consider a successful response, e.g. 204." )
+	flag.StringVar( &flagUseMethod, "method", flagUseMethod, "The HTTP method to use in the request, e.g. GET." )
+	flag.StringVar( &flagProxyServer, "proxy", flagProxyServer, "The IPv4 address & port number of a SOCKS5 proxy server, useful for Tor, e.g. 127.0.0.1:9050." )
+
+	// Set a custom help message
+	flag.Usage = func() {
+		fmt.Printf( "%s, v%s, by %s (%s).\n", PROJECT_NAME, PROJECT_VERSION, AUTHOR_NAME, AUTHOR_WEBSITE )
+		fmt.Printf( "\nUsage: %s [-expect <status>] [-method <method>] [-proxy <IP:PORT>] <URL>\n", os.Args[ 0 ] )
+
+		flag.PrintDefaults()
+
+		os.Exit( 1 ) // By default it exits with code 2
+	}
+
+	// Parse the command-line flags and arguments
+	flag.Parse()
+	argumentUrl := strings.Join( flag.Args(), "" )
+
+	// Ensure the HTTP status code is valid
+	if ( flagExpectStatusCode <= 0 || flagExpectStatusCode >= 1000 ) {
+		fmt.Fprintln( os.Stderr, "Invalid HTTP status code to expect specified, it must be within the range 1 to 999." )
 		os.Exit( 1 )
 	}
 
-	targetUrl, urlParseError := url.Parse( os.Args[ 1 ] )
-	if ( urlParseError != nil ) {
-		fmt.Fprintf( os.Stderr, "Failed to parse URL %s: %s", os.Args[ 1 ], urlParseError.Error() )
+	// Ensure the HTTP method is valid
+	if ( flagUseMethod == "" ) {
+		fmt.Fprintln( os.Stderr, "Invalid HTTP method to use specified, it must not be empty." )
 		os.Exit( 1 )
 	}
 
-	httpClient := &http.Client{ Timeout: 0, CheckRedirect: func( req *http.Request, via []*http.Request ) error {
-		return http.ErrUseLastResponse
-	} }
-
-	httpRequest, httpRequestError := http.NewRequest( http.MethodGet, targetUrl.String(), nil )
-	if ( httpRequestError != nil ) {
-		fmt.Fprintf( os.Stderr, "Failed to create HTTP request: %s", httpRequestError.Error() )
+	// Ensure the target HTTP URL is valid
+	targetUrl, parseError := url.Parse( argumentUrl )
+	if ( parseError != nil ) {
+		fmt.Fprintln( os.Stderr, "Invalid HTTP target URL specified:", parseError.Error() )
+		os.Exit( 1 )
+	} else if ( argumentUrl == "" ) {
+		fmt.Fprintln( os.Stderr, "Invalid HTTP target URL specified, it must not be empty." )
 		os.Exit( 1 )
 	}
 
-	if ( strings.HasSuffix( targetUrl.Host, ".onion" ) ) {
-		proxyAddress := PROXY_ADDRESS
+	// Create a HTTP client that does not follow redirects or timeout
+	httpClient := &http.Client {
+		Timeout: 0,
+		CheckRedirect: func( request *http.Request, via []*http.Request ) error {
+			return http.ErrUseLastResponse
+		},
+	}
 
-		if argumentCount == 2 {
-			proxyAddress = os.Args[ 2 ]
-		}
+	// Create a HTTP request to be sent later on
+	httpRequest, requestError := http.NewRequest( strings.ToUpper( flagUseMethod ), targetUrl.String(), nil )
+	if ( requestError != nil ) {
+		fmt.Fprintln( os.Stderr, "Error creating HTTP request:", requestError.Error() )
+		os.Exit( 1 )
+	}
 
-		proxyDialer, proxyConnectError := proxy.SOCKS5( "tcp", proxyAddress, nil, proxy.Direct )
-		if proxyConnectError != nil {
-			fmt.Fprintf( os.Stderr, "Failed to connect to proxy %s: %s", proxyAddress, proxyConnectError.Error() )
+	// If a proxy server was provided...
+	if ( flagProxyServer != "" ) {
+
+		// Separate the address by IP address and port number
+		flagProxyServerComponents := strings.SplitN( flagProxyServer, ":", 2 )
+
+		// Ensure there are exactly two components in the array
+		if ( len( flagProxyServerComponents ) != 2 ) {
+			fmt.Fprintln( os.Stderr, "Invalid IPv4 address and port combination specified for the proxy server, it should be structured as IP:PORT." )
 			os.Exit( 1 )
 		}
 
-		httpClient.Transport = &http.Transport{ Dial: proxyDialer.Dial }
+		// Ensure the IP address is a valid IPv4 address
+		proxyServerAddress := net.ParseIP( flagProxyServerComponents[ 0 ] )
+		if ( proxyServerAddress == nil || proxyServerAddress.To4() == nil ) {
+			fmt.Fprintln( os.Stderr, "Invalid IPv4 address specified for the proxy server." )
+			os.Exit( 1 )
+		}
+
+		// Ensure the port number is valid
+		proxyServerPort, parseError := strconv.ParseInt( flagProxyServerComponents[ 1 ], 10, 16 )
+		if ( parseError != nil ) {
+			fmt.Fprintln( os.Stderr, "Error parsing port number as an integer:", parseError.Error() )
+			os.Exit( 1 )
+		} else if ( proxyServerPort <= 0 || proxyServerPort >= 65536 ) {
+			fmt.Fprintln( os.Stderr, "Invalid port number specified for the proxy server, it must be within the range 1 to 65535." )
+			os.Exit( 1 )
+		}
+
+		// Create a dialer to the proxy server
+		proxyDialer, proxyError := proxy.SOCKS5( "tcp", fmt.Sprintf( "%s:%d", proxyServerAddress, proxyServerPort ), nil, proxy.Direct )
+		if ( proxyError != nil ) {
+			fmt.Fprintln( os.Stderr, "Error creating dialer to proxy server:", proxyError.Error() )
+			os.Exit( 1 )
+		}
+
+		// Set the HTTP client to use this proxy dialer
+		httpClient.Transport = &http.Transport {
+			Dial: proxyDialer.Dial,
+		}
+
 	}
 
-	httpResponse, httpExecuteError := httpClient.Do( httpRequest )
-	if ( httpExecuteError != nil ) {
-		fmt.Fprintf( os.Stderr, "Failed to execute HTTP request: %s", httpExecuteError.Error() )
+	// Execute the HTTP request using the HTTP client
+	httpResponse, executeError := httpClient.Do( httpRequest )
+	if ( executeError != nil ) {
+		fmt.Fprintln( os.Stderr, "Error executing HTTP request:", executeError.Error() )
 		os.Exit( 1 )
 	}
 
-	httpResponseBody, readBodyError := io.ReadAll( httpResponse.Body )
-	defer httpResponse.Body.Close()
-	if ( readBodyError != nil ) {
-		fmt.Fprintf( os.Stderr, "Failed to read response body: %s", readBodyError.Error() )
+	// The healthcheck failed if the response status code does not match what was expected
+	if ( httpResponse.StatusCode != flagExpectStatusCode ) {
+		fmt.Println( "FAILURE,", httpResponse.Status )
 		os.Exit( 1 )
 	}
 
-	fmt.Printf( "%s: %s, %d bytes", targetUrl.Host, httpResponse.Status, len( httpResponseBody ) )
-
-	if ( httpResponse.StatusCode < 200 || httpResponse.StatusCode > 299 ) {
-		os.Exit( 1 )
-	}
-
+	// If we made it this far then the healthcheck is successful
+	fmt.Println( "SUCCESS,", httpResponse.Status )
 	os.Exit( 0 )
+
 }
